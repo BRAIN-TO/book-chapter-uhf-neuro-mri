c63cb63ecfca9ab645e9e4afc1c8e35964ee4310
Lars Kasper <lars.kasper@rmp.uhn.ca>
2022-03-15 12:28:00 -0400

    Cleaned up SNR plot for release, added traditional Poser2010 plot 3D vs 2D

--- uhfbold_plot_snr_2Dvs3DvsSMS.m
+++ uhfbold_plot_snr_2Dvs3DvsSMS.m
@@ -6,15 +6,22 @@
 %
 %
 %   See also
-%
+% [Poser2010]
 % Poser, B.A., Koopmans, P.J., Witzel, T., Wald, L.L., Barth, M., 2010.
 % Three dimensional echo-planar imaging at 7 Tesla. NeuroImage 51, 261–266.
 % https://doi.org/10.1016/j.neuroimage.2010.01.108
 %
+% [Marques2018]
 % Marques, J.P., Norris, D.G., 2018. How to choose the right MR sequence
 % for your research question at 7T and above? NeuroImage, Neuroimaging with
 % Ultra-high Field MRI: Present and Future 168, 119–140.
 % https://doi.org/10.1016/j.neuroimage.2017.04.044
+%
+% [Stirnberg2020]
+% Educational Talk on "State-of-the-Art Echo-Planar BOLD Acquisition",
+% ISMRM 2020, Abstract E1090 ISMRM Syllabus:
+% https://submissions2.mirasmart.com/ISMRM2020/ViewSubmission.aspx?sbmID=1090&validate=false
+% Video: https://www.youtube.com/watch?v=wuAjYtqI4cY
 
 % Author:   Lars Kasper
 % Created:  2022-03-14
@@ -30,16 +37,16 @@
  
 % T1 Values from [Poser2010]
 T1 = 1.4;
-TR = 70e-3;%;65e-3%70e-3; % TR between consecutive excitations (neighboring slices or re-excitation 3D volume)
-Rz = 4; % maximum assumed undersampling factor in slice direction for SMS (MB factor) and 3D
-FOVz = 120;%100%120;
+TR = 70e-3; %;65e-3%70e-3;  % TR between consecutive excitations (neighboring slices or re-excitation 3D volume)
+Rz = 4;                     % maximum assumed undersampling factor in slice direction for SMS (MB factor) and 3D
+FOVz = 120; %100; %120;
 
 
 idSubject = 'FEINBERGATRON';
 paths = uhfbold_get_paths(idSubject);
 
 doPlotDirectRelativeSnr3DSMSStirnberg = false; %for comparison, should be the same
-doSavePlots = true;
+doSavePlots = false;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Define SNR functions, see derivations in Word document
@@ -47,26 +54,26 @@ doSavePlots = true;
 
 % NOTE: all SNR is rescaled to the same volume TR, i.e., we acquire Rz
 % averages for the SMS and 3D cases (equivalent to scaling with sqrt(TR))
+% SNR_2D and SNR_2D_exp are equivalent, only differ in expression via exp
+% or tanh, with tanh(x) = (exp(2x)-1)/(exp(2x)+1)
 SNR_2D_exp = @(TRvol,T1) (sqrt((1-exp(-TRvol/T1))./(1+exp(-TRvol/T1))));
-SNR_2D = @(TRvol,T1) (Rz*sqrt(tanh(TRvol/(2*T1)))); 
+SNR_2D = @(TRvol,T1) (sqrt(tanh(TRvol/(2*T1)))); 
 
-% WRONG? (vs 2D): Assuming same TR (1 volume of Rz acq only, not 2D), but as in Stirnberg,
-% assuming that SMS has an sqrt(Rz) SNR_0 gain
-% SNR_3D = @(TRvol,T1,nSlices, Rz) (sqrt(nSlices./Rz.*tanh(TRvol.*Rz./(nSlices*2*T1))));
-% SNR_SMS = @(TRvol,T1,nSlices, Rz) (sqrt(Rz.*tanh(TRvol/(Rz*2*T1))));
-
-% CORRECT? (but factor of sqrt(Rz) 3D always better?
-% SNR_3D = @(TRvol,T1,nSlices, Rz) (sqrt(nSlices.*tanh(TRvol.*Rz./(nSlices*2*T1))));
-% SNR_SMS = @(TRvol,T1,nSlices, Rz) (sqrt(Rz.*tanh(TRvol/(Rz*2*T1))));
-
-
-%% Consistent with Stirnberg, TRvol is based on TR from undersampled experiment TR2D/Rz
+% Consistent with Stirnberg, TRvol is based on TR from undersampled experiment TR2D/Rz
 SNR_3D = @(TRvol,T1,nSlices, Rz) (sqrt(nSlices.*tanh(TRvol.*Rz./(nSlices*2*T1))));
 SNR_SMS = @(TRvol,T1,nSlices, Rz) (sqrt(Rz.*tanh(TRvol/(2*T1))));
-% direct formula from Stirnberg ISMRM Educational 2020
+% direct formula from Stirnberg ISMRM Educational 2020, for verification
 rRSNR_3DvsSMS = @(TRvol,T1,nSlices, Rz) (sqrt(nSlices./Rz).*sqrt(tanh(TRvol./(2*T1)*Rz./nSlices)./tanh(TRvol./(2*T1))));
+
+
+colors = [
+     0 0 0
+     0    0.4470    0.7410
+     0.8500    0.3250    0.0980
+     ];
+ 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Plot 2D vs SMS SNR
+%% Plot 2D vs 3D SNR, no undersampling in z (reproduces [Poser2010])
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 stringTitle = {'SNR 2D vs 3D Acquisition', ...
@@ -87,9 +94,20 @@ TRvol = nSlices*TR;
 
 Rz = 1;
 x = FOVz./nSlices;
-y1 = SNR_2D_exp(TRvol, T1);
+y1 = SNR_2D(TRvol, T1);
 y2 = SNR_3D(TRvol, T1, nSlices, Rz);
-hp = plot(x, [y1,y2, y2./y1]);
+
+% subplot vs resolution 
+subplot(2,1,1);
+hp = plot(x, [y1,y2]); hold all;
+hp(1).Color = colors(1,:);
+hp(2).Color = colors(3,:); % to match colors with plot below
+
+
+hp2 = plot(x, y2./y1); hold all;
+hp2(1).Color = hp(2).Color;
+set(hp2, 'LineWidth', 2, 'LineStyle', '-.');
+
 xlim([0, max(x)]);
 ylim([0 3.5])
 set(hp, 'LineWidth', 2);
@@ -97,13 +115,40 @@ legend('SNR 2D', 'SNR 3D', 'SNR 3D/2D');
 xlabel('Resolution (mm)');
 ylabel('Relative SNR');
 title(stringTitle)
-fh.Position = [680   508   866   590];
 grid on
-%fh.Children(1).Position = [ 0.5620    0.3969    0.3233    0.2017]; % legend
+
+% subplot vs nSlices (as in [Poser2010]);
+subplot(2,1,2)
+
+nSlices = (1:150)';
+TRvol = nSlices*TR;
+x = nSlices;
+y1 = SNR_2D(TRvol, T1);
+y2 = SNR_3D(TRvol, T1, nSlices, Rz);
+
+hp = plot(x, [y1,y2]); hold all;
+hp(1).Color = colors(1,:);
+hp(2).Color = colors(3,:); % to match colors with plot below
+
+hp2 = plot(x, y2./y1); hold all;
+hp2(1).Color = hp(2).Color;
+set(hp2, 'LineWidth', 2, 'LineStyle', '-.');
+
+xlim([0, max(x)]);
+ylim([0 2])
+set(hp, 'LineWidth', 2);
+legend('SNR 2D', 'SNR 3D', 'SNR 3D/2D', 'Location', 'SouthEast'); 
+xlabel('nSlices');
+ylabel('Relative SNR');
+title(stringTitle)
+grid on
+
+
+fh.Position = [680   50   866   1000];
 fhArray(1) = fh;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Plot 2D vs SMS SNR
+%% Plot 2D vs 3D vs SMS SNR
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 Rz = 4;
@@ -124,28 +169,26 @@ TRvol = nSlices*TR/Rz; % Volume TR of undersampled case!
 % legend('exp', 'tanh');  
 
 x = FOVz./nSlices;
-y1 = SNR_2D_exp(TRvol*Rz, T1); % volume TR in 2D longer
+y1 = SNR_2D(TRvol*Rz, T1); % volume TR in 2D longer
 y2 = SNR_SMS(TRvol, T1, nSlices, Rz);
 y3 = SNR_3D(TRvol, T1, nSlices, Rz);
 y4 = rRSNR_3DvsSMS(TRvol, T1, nSlices, Rz);
 hp = plot(x, [y1,y2,y3]); hold on
-hp(1).Color = [0 0 0];
-hp(2).Color = [0    0.4470    0.7410];
-hp(3).Color = [0.8500    0.3250    0.0980];
-
+for iPlot=1:numel(hp), hp(iPlot).Color = colors(iPlot,:); end
+ 
 set(hp, 'LineWidth', 2);
 
 if doPlotDirectRelativeSnr3DSMSStirnberg
-hp2 = plot(x, [y2./y1, y3./y1, y3./y2, y4]);
+    hp2 = plot(x, [y2./y1, y3./y1, y3./y2, y4]);
 else
-hp2 = plot(x, [y2./y1, y3./y1, y3./y2]);
-end    
+    hp2 = plot(x, [y2./y1, y3./y1, y3./y2]);
+end
 
 xlim([0, max(x)]);
 ylim([0.5 3.5])
-set(hp2, 'LineWidth', 2, 'LineStyle', '-.');
 
 % match colors of ratios to numerator color
+set(hp2, 'LineWidth', 2, 'LineStyle', '-.');
 hp2(1).Color = hp(2).Color;
 hp2(2).Color = hp(3).Color;
 hp2(3).Color = [0.9290    0.6940    0.1250];
